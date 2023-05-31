0f26434c4d4eb19e43f0968304d81799fe824682
Varuni <nadikuva57@craighead.school.nz>
2023-05-31 15:16:45 +1200

    Finished GST calculator

--- code/2-gst-calculator.js
+++ code/2-gst-calculator.js
@@ -4,4 +4,12 @@
 
 function gstCalculator() {
   alert('The price with GST the price and 15%.')
+
+  let ogPrice = prompt('What is the cost before GST?')
+
+  let finalPrice = ogPrice * 1.15
+
+  alert('The cost with GST is $' +finalPrice)
+
+
 }
