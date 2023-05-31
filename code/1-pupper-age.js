ab915c5be0064d13e7993b7134a77618eb8f82bf
Varuni <nadikuva57@craighead.school.nz>
2023-05-31 15:06:42 +1200

    finished th3e pupper age

--- code/1-pupper-age.js
+++ code/1-pupper-age.js
@@ -6,4 +6,8 @@ function pupperAge() {
   alert('Dog years are human years times seven.')
 
   let humanYears = prompt('How old is your dog in human years?')
+  dogYears = humanYears * 7
+
+  alert('Your dog is ' + dogYears+ ' years old')
 }
+
