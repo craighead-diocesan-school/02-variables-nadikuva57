8afaef46e429672ef4e1eae400063b9c7d4a30ef
Varuni <nadikuva57@craighead.school.nz>
2023-07-17 06:56:22 +1200

    Update

--- code/3-lessons-to-seconds.js
+++ code/3-lessons-to-seconds.js
@@ -4,4 +4,8 @@
 
 function lessonsToSeconds() {
   alert('There are 60 minutes in a lesson, and 60 seconds in a minute.')
+
+  let lessonsLeft = prompt('How many lessons are left in the day?')
+  let secondsLeft = lessonsLeft*60*60
+  alert('There are' +secondsLeft+ 'Secons in left in the day')
 }
--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,7 @@
 // ####################################
 
 function lifeRemaining() {
-  
+  let age = prompt('How old are you?')
+  let yearsLeft = 85.5 - age
+  alert('You have' +yearsLeft+ 'years left to live')
 }
